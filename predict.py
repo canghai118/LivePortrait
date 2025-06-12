4b78f7a9d0603ac20f5893433ed9ef9c2e259a21
canghai118 <zjy71055@gmail.com>
2025-06-12 06:49:47 +0000

    with cog

--- predict.py
+++ predict.py
@@ -16,10 +16,11 @@ class Predictor(BasePredictor):
         driving_video: Path = Input(description="Driving video for animation"),
     ) -> Path:
         """Run a single prediction on the model"""
-        image_filename = source_image.name
-        video_filename = driving_video.name
+        image_filename = source_image.stem
+        video_filename = driving_video.stem
 
         output_filename = f"{image_filename}--{video_filename}.mp4"
+        print("output_filename------", output_filename)
 
         # 执行命令行程序
         cmd = [
